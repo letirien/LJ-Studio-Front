e0bd8d6708a0d97801b125f137029523d168a746
Titien <titien.schott.67@gmail.com>
2026-03-11 14:45:02 +0100

    fix: collab bottom space

--- components/home/Collab.js
+++ components/home/Collab.js
@@ -141,7 +141,7 @@ const Collab = ({logos}) => {
   }, [onLoadingComplete]);
 
   return (
-    <div className="section-collab sm:mt-24 pb-32 relative z-3 bg-black" ref={containerRef}>
+    <div className="section-collab sm:mt-24 pb-12 sm:pb-32 relative z-3 bg-black" ref={containerRef}>
       {/* Marquee pour la rangée du haut */}
       <div 
         className="marquee-advanced" 
